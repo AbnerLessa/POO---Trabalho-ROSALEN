495dbace43d4ac4a0035f58266356e2604d38bf8
AbnerLessa <111578881+AbnerLessa@users.noreply.github.com>
2023-10-09 10:45:33 -0300

    Add files via upload

---
+++ IPTU vv/Imovel.java
@@ -0,0 +1,25 @@
+public class Imovel{
+    String nome;
+    float imposto;
+    int Meses_atraso;
+
+    public String getNome(){
+        return nome;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public float getImposto(){
+        return imposto;
+    }
+    public void setImposto(float imposto){
+        this.imposto = imposto;
+    }
+    public int getMeses_atraso(){
+        return Meses_atraso;
+    }
+    public void setMeses_atraso(int Meses_atraso){
+        this.Meses_atraso = Meses_atraso;
+    }
+    
+}
---
+++ IPTU vv/Municipio.java
@@ -0,0 +1,19 @@
+public class Municipio{
+    String nome;
+
+    int idade;
+    
+    
+    List<Imovel> lim;
+
+
+void cadastrarImovel(Imovel im){
+    lim.add(1,im);
+    lim.add(2,im);
+
+}
+
+
+
+
+}
