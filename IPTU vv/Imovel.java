ce6f393c0919d57954b9e046db5a66fa451d565d
AbnerLessa <111578881+AbnerLessa@users.noreply.github.com>
2023-10-16 07:52:48 -0300

    Imovel.java

--- IPTU vv/Imovel.java
+++ IPTU vv/Imovel.java
@@ -13,15 +13,15 @@ class Imovel {
         double multa = 0.0;
 
         if (mesesAtraso >= 0 && mesesAtraso <= 5) {
-            multa = imposto * 0.01; // 1% de multa
+            multa = imposto * 0.01;
         } else if (mesesAtraso >= 6 && mesesAtraso <= 8) {
-            multa = imposto * 0.023; // 2.3% de multa
+            multa = imposto * 0.023; 
         } else if (mesesAtraso >= 9 && mesesAtraso <= 10) {
-            multa = imposto * 0.03; // 3% de multa
+            multa = imposto * 0.03; 
         } else if (mesesAtraso >= 11 && mesesAtraso <= 12) {
-            multa = imposto * 0.054; // 5.4% de multa
+            multa = imposto * 0.054; 
         } else {
-            multa = imposto * 0.1; // 10% de multa para atraso superior a 12 meses
+            multa = imposto * 0.1; 
         }
 
         return multa;
