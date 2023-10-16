6993267327aeb5b904cb6bf02e811942222524cd
AbnerLessa <111578881+AbnerLessa@users.noreply.github.com>
2023-10-16 07:38:06 -0300

    Imovel.java

--- IPTU vv/Imovel.java
+++ IPTU vv/Imovel.java
@@ -1,25 +1,29 @@
-public class Imovel{
-    String nome;
-    float imposto;
-    int Meses_atraso;
+class Imovel {
+    private String nomeProprietario;
+    private double imposto;
+    private int mesesAtraso;
 
-    public String getNome(){
-        return nome;
-    }
-    public void setNome(String nome){
-        this.nome = nome;
-    }
-    public float getImposto(){
-        return imposto;
-    }
-    public void setImposto(float imposto){
+    public Imovel(String nomeProprietario, double imposto, int mesesAtraso) {
+        this.nomeProprietario = nomeProprietario;
         this.imposto = imposto;
+        this.mesesAtraso = mesesAtraso;
     }
-    public int getMeses_atraso(){
-        return Meses_atraso;
+
+    public double calcularMulta() {
+        double multa = 0.0;
+
+        if (mesesAtraso >= 0 && mesesAtraso <= 5) {
+            multa = imposto * 0.01; // 1% de multa
+        } else if (mesesAtraso >= 6 && mesesAtraso <= 8) {
+            multa = imposto * 0.023; // 2.3% de multa
+        } else if (mesesAtraso >= 9 && mesesAtraso <= 10) {
+            multa = imposto * 0.03; // 3% de multa
+        } else if (mesesAtraso >= 11 && mesesAtraso <= 12) {
+            multa = imposto * 0.054; // 5.4% de multa
+        } else {
+            multa = imposto * 0.1; // 10% de multa para atraso superior a 12 meses
+        }
+
+        return multa;
     }
-    public void setMeses_atraso(int Meses_atraso){
-        this.Meses_atraso = Meses_atraso;
-    }
-    
 }
