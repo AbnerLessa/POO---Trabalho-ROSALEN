6504dfcff0b5498544fe0e53e0e9ebbae9ebb187
AbnerLessa <111578881+AbnerLessa@users.noreply.github.com>
2023-10-16 07:42:02 -0300

    Municipio.java

--- IPTU vv/Municipio.java
+++ IPTU vv/Municipio.java
@@ -1,19 +1,22 @@
-public class Municipio{
-    String nome;
+class Municipio {
+    private List<Imovel> imoveis;
 
-    int idade;
-    
-    
-    List<Imovel> lim;
+    public Municipio() {
+        imoveis = new ArrayList<>();
+    }
 
+    public void adicionarImovel(Imovel imovel) {
+        imoveis.add(imovel);
+    }
 
-void cadastrarImovel(Imovel im){
-    lim.add(1,im);
-    lim.add(2,im);
-
-}
-
-
-
-
+    public void calcularMultaImoveis() {
+        for (Imovel imovel : imoveis) {
+            double multa = imovel.calcularMulta();
+            System.out.println("Proprietário: " + imovel.nomeProprietario);
+            System.out.println("Valor do imposto: " + imovel.imposto);
+            System.out.println("Meses de atraso: " + imovel.mesesAtraso);
+            System.out.println("Multa a ser paga: " + multa);
+            System.out.println();
+        }
+    }
 }
